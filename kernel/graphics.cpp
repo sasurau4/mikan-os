0efb182250ee9f731f001200968afa99ea5e5648
Daiki Ihara <sasurau4@gmail.com>
2025-08-07 21:20:41 +0900

    feat: finished chapter 5.3

---
+++ kernel/graphics.cpp
@@ -0,0 +1,23 @@
+
+/**
+ * @file graphics.cpp
+ * @brief Graphics-related functions and classes
+ */
+
+#include "graphics.hpp"
+
+void RGBResv8BitPerColorPixelWriter::Write(int x, int y, const PixelColor &c)
+{
+    auto p = PixelAt(x, y);
+    p[0] = c.r;
+    p[1] = c.g;
+    p[2] = c.b;
+}
+
+void BGRResv8BitPerColorPixelWriter::Write(int x, int y, const PixelColor &c)
+{
+    auto p = PixelAt(x, y);
+    p[0] = c.b;
+    p[1] = c.g;
+    p[2] = c.r;
+}
