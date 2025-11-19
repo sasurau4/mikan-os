c7140dffa2649afe57aeaad64a790582dd3102e7
Daiki Ihara <sasurau4@gmail.com>
2025-11-19 21:33:54 +0900

    fix: infinite loop of mouse cursor

--- kernel/graphics.hpp
+++ kernel/graphics.hpp
@@ -45,6 +45,12 @@ Vector2D<T> ElementMax(const Vector2D<T> &lhs, const Vector2D<T> &rhs)
 }
 
 template <typename T>
+Vector2D<T> ElementMin(const Vector2D<T> &lhs, const Vector2D<T> &rhs)
+{
+    return {std::min(lhs.x, rhs.x), std::min(lhs.y, rhs.y)};
+}
+
+template <typename T>
 struct Rectangle
 {
     Vector2D<T> pos, size;
