f77a39e35663c370479c3ab29c6b76b355752cb1
Daiki Ihara <sasurau4@gmail.com>
2025-12-20 22:50:43 +0900

    feat: add idle task at lowest level

--- kernel/task.cpp
+++ kernel/task.cpp
@@ -16,6 +16,14 @@ namespace
     }
 }
 
+void TaskIdle(uint64_t task_id, int64_t data)
+{
+    while (true)
+    {
+        __asm__("hlt");
+    }
+}
+
 Task::Task(uint64_t id) : id_{id}, msgs_{}
 {
 }
@@ -89,6 +97,12 @@ TaskManager::TaskManager()
                      .SetLevel(current_level_)
                      .SetRunning(true);
     running_[current_level_].push_back(&task);
+
+    Task &idle = NewTask()
+                     .InitContext(TaskIdle, 0)
+                     .SetLevel(0)
+                     .SetRunning(true);
+    running_[0].push_back(&idle);
 }
 
 Task &TaskManager::NewTask()
