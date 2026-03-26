6149babf97ce0cc600682c5528bb4cfc4ca92cae
Daiki Ihara <sasurau4@gmail.com>
2026-03-26 23:15:21 +0900

    feat: impl kMouseMove

--- kernel/app_event.hpp
+++ kernel/app_event.hpp
@@ -10,7 +10,18 @@ extern "C"
         enum Type
         {
             kQuit,
+            kMouseMove,
         } type;
+
+        union
+        {
+            struct
+            {
+                int x, y;
+                int dx, dy;
+                uint8_t buttons;
+            } mouse_move;
+        } arg;
     };
 #ifdef __cplusplus
 } // extern "C"
--- kernel/message.hpp
+++ kernel/message.hpp
@@ -17,6 +17,7 @@ struct Message
         kKeyPush,
         kLayer,
         kLayerFinish,
+        kMouseMove,
     } type;
 
     uint64_t src_task;
@@ -43,5 +44,12 @@ struct Message
             int x, y;
             int w, h;
         } layer;
+
+        struct
+        {
+            int x, y;
+            int dx, dy;
+            uint8_t buttons;
+        } mouse_move;
     } arg;
 };
