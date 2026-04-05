67cfe9071d8332ad14e2abb1b967b021454c8408
Daiki Ihara <sasurau4@gmail.com>
2026-04-05 23:34:13 +0900

    feat: impl blink cursor by each terminal

--- kernel/layer.cpp
+++ kernel/layer.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include "console.hpp"
 #include "logger.hpp"
+#include "task.hpp"
 
 namespace
 {
@@ -247,6 +248,19 @@ int LayerManager::GetHeight(unsigned int id)
 namespace
 {
     FrameBuffer *screen;
+
+    Error SendWindowActiveMessage(unsigned int layer_id, int activate)
+    {
+        auto task_id = layer_task_map->find(layer_id);
+        if (task_id == layer_task_map->end())
+        {
+            return MAKE_ERROR(Error::kNoSuchTask);
+        }
+
+        Message msg{Message::kWindowActive};
+        msg.arg.window_active.activate = activate;
+        return task_manager->SendMessage(task_id->second, msg);
+    }
 }
 
 LayerManager *layer_manager;
@@ -270,6 +284,7 @@ void ActiveLayer::Activate(unsigned int layer_id)
         Layer *layer = manager_.FindLayer(active_layer_);
         layer->GetWindow()->Deactivate();
         manager_.Draw(active_layer_);
+        SendWindowActiveMessage(active_layer_, 0);
     }
 
     active_layer_ = layer_id;
@@ -279,6 +294,7 @@ void ActiveLayer::Activate(unsigned int layer_id)
         layer->GetWindow()->Activate();
         manager_.UpDown(active_layer_, manager_.GetHeight(mouse_layer_) - 1);
         manager_.Draw(active_layer_);
+        SendWindowActiveMessage(active_layer_, 1);
     }
 }
 
