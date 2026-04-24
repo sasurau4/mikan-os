e4f152ce7357c0d5caa06e86fe881c517f2ccdfd
Daiki Ihara <sasurau4@gmail.com>
2026-04-24 22:23:54 +0900

    feat: impl pipe

--- kernel/message.hpp
+++ kernel/message.hpp
@@ -20,6 +20,7 @@ struct Message
         kMouseMove,
         kMouseButton,
         kWindowActive,
+        kPipe,
     } type;
 
     uint64_t src_task;
@@ -66,5 +67,11 @@ struct Message
         {
             int activate; // 1: activate, 0: deactivate
         } window_active;
+
+        struct
+        {
+            char data[16];
+            uint8_t len;
+        } pipe;
     } arg;
 };
