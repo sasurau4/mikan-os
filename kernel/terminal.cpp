c4b08fdaee93257d0e93e5944f480de2fe316c08
Daiki Ihara <sasurau4@gmail.com>
2026-04-25 17:15:49 +0900

    feat: impl more command

--- kernel/terminal.cpp
+++ kernel/terminal.cpp
@@ -562,6 +562,7 @@ void Terminal::ExecuteLine()
                                       reinterpret_cast<int64_t>(term_desc))
                          .Wakeup()
                          .ID();
+        (*layer_task_map)[layer_id_] = subtask_id;
     }
 
     if (strcmp(command, "echo") == 0)
@@ -717,6 +718,7 @@ void Terminal::ExecuteLine()
         pipe_fd->FinishWrite();
         __asm__("cli");
         auto [ec, err] = task_manager->WaitFinish(subtask_id);
+        (*layer_task_map)[layer_id_] = task_.ID();
         __asm__("sti");
         if (err)
         {
